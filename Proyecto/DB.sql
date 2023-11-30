177cb65859127a962882476dff06ba5e1a9d016f
davidvivascamargo <jesusdavid.vc98@gmail.com>
2023-11-30 16:40:24 -0600

    Mejorando conexion.php

--- Proyecto/DB.sql
+++ Proyecto/DB.sql
@@ -2,7 +2,7 @@ DROP SCHEMA IF EXISTS registros;
 
 CREATE SCHEMA IF NOT EXISTS  registros DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
 
-USE registros;
+USE registros; 
 
 CREATE TABLE usuarios (
   fullName text COLLATE utf8mb4_spanish2_ci NOT NULL,
@@ -10,10 +10,7 @@ CREATE TABLE usuarios (
   email text COLLATE utf8mb4_spanish2_ci NOT NULL,
   phoneNumber varchar(15) COLLATE utf8mb4_spanish2_ci NOT NULL,
   password varchar(255) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  password varchar(255) COLLATE utf8mb4_spanish2_ci NOT NULL,
-  Permisos int(11) NOT NULL DEFAULT '1',
-  id int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
-INSERT INTO usuarios VALUES ('Aaron Velasco Agustin','Huron','aaronvelasco@aragon.unam.mx', '5626198295','123456','123456',1,1);
+INSERT INTO usuarios VALUES ('Aaron Velasco Agustin','Huron','aaronvelasco@aragon.unam.mx', '5626198295','123456',1);
--- Proyecto/conexion.php
+++ Proyecto/conexion.php
@@ -4,9 +4,39 @@ $user_db = "root";
 $pass_db = "12345678";
 $db_name = "registros";
 
-$conexion = new mysqli($host_db, $user_db, $pass_db, $db_name);
 
-if ($conexion->connect_error) {
- die("La conexion falló jiji: " . $conexion->connect_error);
+// Crear Conexión
+$conexion = new mysqli ($host_db $user_db, $pass_db, $db_name);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $sql = "INSERT INTO user (username, password)
+  values ('$username','$password')";
+  if ($conn->query($sql)){
+    echo "El nuevo registro se insertó correctamente";
+  }
+  else{
+    echo "Error: ". $sql ."
+". $conn->error;
+  }
+  $conn->close();
+}
+}
+else{
+  echo "La contraseña no debe estar vacía";
+  die();
+}
+ }
+ else{
+  echo "El nombre de usuario no debe estar vacío";
+  die();
+ }
+ if($username !=''&& $password !='')
+{
+// Para redirigir el formulario en una página en particular
+header("Location:https://C:\Users\david\OneDrive\Documentos\DESARROLLO\WS_ProgramacionWeb\Proyecto\Login.html");
 }
 ?>
