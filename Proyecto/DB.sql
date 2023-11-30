6fa8f93ad85fa65229859ea7300003537cef4496
davidvivascamargo <jesusdavid.vc98@gmail.com>
2023-11-30 17:52:37 -0600

    Correcciones conexion.php

--- Proyecto/DB.sql
+++ Proyecto/DB.sql
@@ -10,7 +10,8 @@ CREATE TABLE usuarios (
   email text COLLATE utf8mb4_spanish2_ci NOT NULL,
   phoneNumber varchar(15) COLLATE utf8mb4_spanish2_ci NOT NULL,
   password varchar(255) COLLATE utf8mb4_spanish2_ci NOT NULL,
+  password2 varchar(255) COLLATE utf8mb4_spanish2_ci NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
-INSERT INTO usuarios VALUES ('Aaron Velasco Agustin','Huron','aaronvelasco@aragon.unam.mx', '5626198295','123456',1);
+/*INSERT INTO usuarios VALUES ('Aaron Velasco Agustin','Huron','aaronvelasco@aragon.unam.mx', '5626198295','123456',1);
---
+++ Proyecto/Registration.php
@@ -0,0 +1,65 @@
+<?php
+
+$fullName = filter__input(INPUT_POST,)
+$userName = filter_input(INPUT_POST, 'userName');
+$email =filter_input(INPUT_POST,'email');
+$phoneNumber =filter_input(INPUT_POST,'phoneNumber')
+$password = filter_input(INPUT_POST, 'password');
+$password2 =filter_input(INPUT_POST, 'password2');
+
+if (!empty($userName)){
+if (!empty($email)){
+if (!empty($password)){
+if (!empty($password2)){
+
+
+$host_db = "localhost:3306";
+$user_db = "root";
+$pass_db = "12345678";
+$db_name = "registros";
+
+// Create connection
+$conexion = new mysqli ($host_db, $user_db, $pass_db, $db_name);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+if($password!=$password2)
+  {
+	  echo "La contraseña no coincide";
+  }
+else{
+$sql = "INSERT INTO user (userName, email, password, password2)
+  values ('$userName','$email','$password','$password2')";
+}
+  if ($conexion ->query($sql)){
+    echo "El nuevo registro se insertó correctamente";	
+  }
+  else{
+    echo "Error: ". $sql ."
+". $conexion ->error;
+  }
+  
+  $conexion ->close();
+}
+else
+{
+  echo "la contraseña no debe estar vacía";
+  die();
+}
+}
+else
+{
+  echo "el correo electrónico no debe estar vacío";
+  die();
+}
+ }
+ else
+ {
+  echo "El nombre de usuario no debe estar vacío.";
+  die();
+ }
+}
+
+ ?>
